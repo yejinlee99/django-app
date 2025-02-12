1c310f25c49c3c606e508d03bae84fd6ecc3a9e1
playdata\playdata2 <yejin3388@naver.com>
2025-02-12 10:48:36 +0900

    Feat: dev/prod 실행환경 분리

--- django_app/settings.py
+++ django_app/settings.py
@@ -25,10 +25,7 @@ SECRET_KEY = 'django-insecure-qa_=+$5+uu6w5kmt0-wcr=e2053(k&g8!%yy@sk=&f0r-g!&(r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    'localhost',
-    '52.79.67.115',
-]
+ALLOWED_HOSTS = []
 
 
 # Application definition
